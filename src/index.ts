311106f968d0db92edc27b83c22644608e186da5
Aman Dahiya <amandahiyabuss@gmail.com>
2025-05-01 15:30:33 +0530

    fix: updated files

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export function greet(name: string) {
+  return `Hello hello, ${name}!`;
+}
+
+export function add(a: number, b: number) {
+  const d = a + b;
+  return d;
+}
+
+console.log("helloo");
